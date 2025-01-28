881ddde4c19e46dd0f2ee2cce5415b656c1938d4
unknown <zxckendrick123@gmail.com>
2025-01-28 10:09:20 +0800

    Fixed dashboard display

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,14 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import Page from "./dashboard/page";
 
 export default function Layout({ children }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
         {children}
-      </main>
+        <Page />
     </SidebarProvider>
   );
 }
